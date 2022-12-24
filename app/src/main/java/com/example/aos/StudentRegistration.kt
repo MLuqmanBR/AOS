2933e043dab737f59f433a9b35ec0072079e3277
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-24 20:52:54 +0530

    m

--- app/src/main/java/com/example/aos/StudentRegistration.kt
+++ app/src/main/java/com/example/aos/StudentRegistration.kt
@@ -1,18 +1,26 @@
 package com.example.aos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.*
 
 class StudentRegistration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_registration)
 
+        val button_3 = findViewById<Button>(R.id.button3)
+        button_3.setOnClickListener({
+            val intent = Intent(this,Attendance::class.java)
+            startActivity(intent)
+        })
 
+        val button_4 = findViewById<Button>(R.id.button4)
+        button_4.setOnClickListener({
+            val intent = Intent(this,AdminSignup::class.java)
+            startActivity(intent)
+        })
         }
 }
