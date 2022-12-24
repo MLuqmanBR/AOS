91d74c09444dc68506b70c3bc0252bcc425f7b88
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-24 20:33:09 +0530

    new

--- app/src/main/java/com/example/aos/AdminSignup.kt
+++ app/src/main/java/com/example/aos/AdminSignup.kt
@@ -2,10 +2,25 @@ package com.example.aos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
 
 class AdminSignup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_signup)
+
+        val button_1 = findViewById<Button>(R.id.button1)
+        button_1.setOnClickListener({
+            val intent = Intent(this,Attendance::class.java)
+            startActivity(intent)
+        })
+
+        val button_2 = findViewById<Button>(R.id.button2)
+        button_2.setOnClickListener({
+            val intent = Intent(this,StudentRegistration::class.java)
+            startActivity(intent)
+        })
     }
 }
