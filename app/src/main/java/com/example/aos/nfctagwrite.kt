741701a628e845a0d9476f320af0cc9e02aefb93
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-27 15:13:19 +0530

    updated NFC tag writer

--- app/src/main/java/com/example/aos/nfctagwrite.kt
+++ app/src/main/java/com/example/aos/nfctagwrite.kt
@@ -7,11 +7,11 @@ import android.nfc.*
 import android.nfc.tech.Ndef
 import android.nfc.tech.NdefFormatable
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import java.io.IOException
 
 class nfctagwrite : AppCompatActivity() {
@@ -38,9 +38,19 @@ class nfctagwrite : AppCompatActivity() {
         rollNoEditText = findViewById(R.id.roll_no)
         BranchEditText = findViewById(R.id.Branch)
 
+
+
         val button3 = findViewById<Button>(R.id.button3)
-        button3.setOnClickListener({})
-            // Set up the NFC adapter
+        button3.setOnClickListener {
+            // Write the NDEF message to the NFC tag
+
+            val ndefMessage = null
+            val tag = null
+
+            writeNdefMessageToTag(ndefMessage, tag)
+
+        }
+        // Set up the NFC adapter
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
         // Set up a pending intent to handle NFC tag scanning
@@ -56,6 +66,38 @@ class nfctagwrite : AppCompatActivity() {
         nfcAdapter.enableForegroundDispatch(
             this, pendingIntent, filters, techList
         )
+    }
+
+
+    fun writeNdefMessageToTag(ndefMessage: Nothing?, tag: Tag?) {
+        try {
+            // Get an NDEF object from the NFC tag
+            val ndef = Ndef.get(tag)
+
+            // Connect to the NFC tag
+            ndef.connect()
+
+            // Write the NDEF message to the NFC tag
+            ndef.writeNdefMessage(ndefMessage)
+
+            // Close the connection to the NFC tag
+            ndef.close()
+
+            // Show a toast message to confirm that the NDEF message was written to the NFC tag
+            Toast.makeText(this, "NDEF message written to NFC tag.", Toast.LENGTH_SHORT)
+                .show()
+        } catch (e: IOException) {
+            // Show a toast message if there was an error writing the NDEF message to the NFC tag
+            Toast.makeText(
+                this,
+                "Error writing NDEF message to NFC tag.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } catch (e: FormatException) {
+            // Show a toast message if there was an error formatting the NDEF message
+            Toast.makeText(this, "Error formatting NDEF message.", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -86,35 +128,10 @@ class nfctagwrite : AppCompatActivity() {
                 )
             )
 
-            // Write the NDEF message to the NFC tag
-            writeNdefMessageToTag(ndefMessage, tag)
+
         }
     }
 
 
 
-    private fun writeNdefMessageToTag(ndefMessage: NdefMessage, tag: Tag?) {
-        try {
-            // Get an NDEF object from the NFC tag
-            val ndef = Ndef.get(tag)
-
-            // Connect to the NFC tag
-            ndef.connect()
-
-            // Write the NDEF message to the NFC tag
-            ndef.writeNdefMessage(ndefMessage)
-
-            // Close the connection to the NFC tag
-            ndef.close()
-
-            // Show a toast message to confirm that the NDEF message was written to the NFC tag
-            Toast.makeText(this, "NDEF message written to NFC tag.", Toast.LENGTH_SHORT).show()
-        } catch (e: IOException) {
-            // Show a toast message if there was an error writing the NDEF message to the NFC tag
-            Toast.makeText(this, "Error writing NDEF message to NFC tag.", Toast.LENGTH_SHORT).show()
-        } catch (e: FormatException) {
-            // Show a toast message if there was an error formatting the NDEF message
-            Toast.makeText(this, "Error formatting NDEF message.", Toast.LENGTH_SHORT).show()
-        }
-    }
 }
